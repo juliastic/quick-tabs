4e6a7f25196ebc8300654ad8f11c9f279e2f84d9
juliastic <jusogrill@gmail.com>
2016-11-06 10:22:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple script to open multiple YouTube links from a file
---
+++ quickTabs.py
@@ -0,0 +1,18 @@
+import webbrowser, time, sys, argparse, os
+
+parser = argparse.ArgumentParser()    
+parser.add_argument('file')
+
+args = parser.parse_args()  
+file = args.file
+if os.stat(file).st_size == 0:
+	sys.exit('Cannot parse empty text file')
+
+with open(file) as url_file:
+    urls = [line.strip() for line in url_file]
+webbrowser.open(urls[0])
+time.sleep(1)
+for url in urls[1:]:
+    webbrowser.open_new_tab(url)
+
+file.close()
